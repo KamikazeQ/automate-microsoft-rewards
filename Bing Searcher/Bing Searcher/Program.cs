459e2946f29b6a0a3927e93ab1f6ab5c0b93ddb8
Quinton Gordon <qegordonjr@email.phoenix.edu>
2019-08-13 21:27:03 -0400

    Initial Commit
    First Commit

---
+++ Bing Searcher/Bing Searcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace Bing_Searcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> searchWords = new List<string>() {"cat", "ai", "duck", "hero", "dog", "girl", "man", "cow", "clown", "woman",
+              "tree", "building", "bike", "motorcycle", "crimefighter", "shoe", "plane", "pig", "mop", "broom",
+              "plant", "kitty", "bang", "butt", "ball", "scooter", "tube", "mouse", "dart", "gun",
+              "goose", "bear", "television", "window"};
+
+            // Opens IE Edge
+            /*
+            using (IWebDriver driver = new EdgeDriver())
+            {
+                driver.Navigate().GoToUrl("http://www.bing.com/");
+            }
+            */
+
+            IWebDriver driver = new EdgeDriver();
+
+            driver.Navigate().GoToUrl(@"http://www.bing.com");
+
+            // Search
+
+            foreach (string word in searchWords)
+            {
+                IWebElement elem = driver.FindElement(By.Name("q"));
+                elem.Clear();
+                Thread.Sleep(2000);
+                elem.SendKeys("What is a " + word + "?");
+                Thread.Sleep(2000);
+                elem.SendKeys(Keys.Return);
+                Thread.Sleep(3000);
+            }
+
+            Thread.Sleep(2000);
+            driver.Close();
+        }
+    }
+}
